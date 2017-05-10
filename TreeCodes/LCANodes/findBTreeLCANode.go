63efaa5182e6248e26468557413060ad4a002324
jerana <jeevan.rana@gmail.com>
2017-05-10 16:52:02 -0700

    update LCA code

--- TreeCodes/LCANodes/findBTreeLCANode.go
+++ TreeCodes/LCANodes/findBTreeLCANode.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+/*Find LCA of Binary Tree*/
+/*Algo 1 : Recursive*/
 
+func findLCABTree(root, n1, n2 *TreeNode) *TreeNode {
+	//If root is nil or one of node match with root
+	if !root || root == n1 || root == n2 {
+		return root
+	}
+	//Case 2 : Find if any of given nodes exist in left subtree and right subtree of given tree
+	lSubtree := findLCABTree(root.Left, n1, n2)
+	rSubtree := findLCABTree(root.Right, n1, n2)
+	//If both are non-nil means given nodes fall into different tree
+	if lSubtree && rSubtree {
+		return root
+	}
+	//If both are nil means nodes doesn't belong to tree
+	if !rSubtree && !lSubtree {
+		return nil
+	}
+	if rSubtree != nil {
+		return rSubtree
+	}
+	return lSubtree
+}
+
+/*Algo 2 : Path trace solution */
 func findPath(root, n *TreeNode, nList []*TreeNode) bool {
 	if root == nil {
 		return false
